610e35392c894f64300cacaa2e4065f1a95d3e87
Piotr Balwierczak <pbalwierczak@gmail.com>
2017-07-14 22:31:29 +0200

    Testing Firebase to retrieve data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  symbols: FirebaseListObservable<any[]>;
+  user: Observable<firebase.User>;
+
+  constructor(db: AngularFireDatabase, public afAuth: AngularFireAuth) {
+	
+  	this.symbols = db.list('/stockSymbols');
+  	this.user = afAuth.authState;
+
+  	this.login();
+  }
+
+  login() {
+    this.afAuth.auth.signInAnonymously();
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  generateArray(obj){
+	return Object.keys(obj).map((key)=>{ return obj[key]});
+  }
 }
